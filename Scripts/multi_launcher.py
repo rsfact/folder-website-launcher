0ce4c660c71fe3552fdff2812f09adb5328e776f
ryoheiszk <63033551+ryoheiszk@users.noreply.github.com>
2024-04-30 02:22:11 +0900

    2nd

--- Scripts/multi_launcher.py
+++ Scripts/multi_launcher.py
@@ -4,30 +4,31 @@ import os
 import sys
 
 def launch_customer_sites(json_path, customer_code):
-    with open(json_path, 'r', encoding='utf-8') as file:
+    with open(json_path, "r", encoding="utf-8") as file:
         data = json.load(file)
 
     # 顧客名に基づいて情報を探す
     found = False
     for customer in data:
-        if customer['code'] == customer_code:
+        if customer["code"] == customer_code:
             found = True
             # 関連するウェブサイトを開く
-            for site in customer['sites']:
+            for site in customer["sites"]:
                 webbrowser.open(site)
             # 関連するフォルダを開く
-            for folder in customer['folders']:
+            for folder in customer["folders"]:
                 os.startfile(folder)
+                print(folder)
             break
 
     if not found:
         print("指定された顧客名のデータが見つかりません。")
 
         import subprocess
-        subprocess.call('PAUSE', shell=True)
+        subprocess.call("PAUSE", shell=True)
 
 
 if __name__ == "__main__":
     json_path = sys.argv[1]
-    customer_name = sys.argv[2]
-    launch_customer_sites(json_path, customer_name)
+    customer_code = sys.argv[2]
+    launch_customer_sites(json_path, customer_code)
